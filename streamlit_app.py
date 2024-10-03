c0c60682b6b95519c496a562727e3c8b44b0f820
Onkar Shahapurkar <163027200+Onkar-Shahapurkar@users.noreply.github.com>
2024-10-03 22:26:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -130,7 +130,7 @@ elif selection == "📝 Assignment 1":
         img_persona3 = "https://raw.githubusercontent.com/Onkar-Shahapurkar/HCI-Project/main/Priya Kapoor.png"  # Update this with the actual image URL
         st.image(img_persona3, caption="Priya Kapoor", width=150)
 
-    col4, col5 = st.columns(1, 1])
+    col4, col5 = st.columns([1, 1])
 
     with col4:
         img_persona4 = "https://raw.githubusercontent.com/Onkar-Shahapurkar/HCI-Project/main/Maya Desai.png"  # Update this with the actual image URL
